0fdb41cc6a22c4c78cc281eca9059337b2ccd3ab
Colin Powell <colin@onecardinal.com>
2011-03-28 08:57:13 -0400

    Still working on getting the basics in there, but at least most urls work now.

---
+++ eventy/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from eventy.models import *
+
+class EventTimeAdmin(admin.ModelAdmin):
+    list_display = ('event', 'start', 'end', 'place', )
+    search_fields = ('event',)
+    list_filter = ( 'place',  )
+
+admin.site.register(EventTime, EventTimeAdmin)
+    
+admin.site.register(Place)
+admin.site.register(Calendar)
+admin.site.register(Event)

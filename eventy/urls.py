968c32b903bb090dc16d24a4ec407dd3f44cdb52
Colin Powell <colin@onecardinal.com>
2011-03-28 16:44:01 -0400

    Fixing urls so that they actually work (i.e., adding url before them so we can use kwargs).

--- eventy/urls.py
+++ eventy/urls.py
@@ -1,29 +1,25 @@
 from django.conf.urls.defaults import *
-from django.views.generic import ListView
-from django.views.generic.date import 
-from django.conf import settings
+from django.views.generic import ListView, DetailView
 from eventy.models import Event, EventTime, Calendar, Place
 from eventy.views import EventDetailView, EventDayView, EventMonthView, EventYearView, EventListView
 
 urlpatterns = patterns('',
-    (r'^(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', EventDetailView.as_view(), name='event-detail'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',                  EventDayView.as_view(),    name='events-day'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',                                   EventMonthView.as_view(),  name='events-month'),
-    (r'^(?P<year>\d{4})/$',                                                      EventYearView.as_view(),   name='events-year'),
-    
-    (r'^(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/$',                  EventMonthView.as_view(),  name='event-month'),
-    (r'^(?P<slug>[-\w]+)/(?P<year>\d{4})/$',                                     EventYearView.as_view(),   name='event-year'),
-    (r'^(?P<slug>[-\w]+)/$',                                                     DetailView.as_view(model=Event), name='event-info'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',                view=EventDayView.as_view(),    name='events-day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',                                 view=EventMonthView.as_view(),  name='events-month'),
+    url(r'^(?P<year>\d{4})/$',                                                    view=EventYearView.as_view(),   name='events-year'),
 
-    (r'^calendars/(?P<cal_slug>[-\w]+)/(?P<year>\d{1,2})/$',                     EventYearView.as_view(),   name='event-calendar-year'),
-    (r'^calendars/(?P<cal_slug>[-\w]+)/(?P<year>\d{1,2})/(?P<month>\d{1,2})/$',  EventMonthView.as_view(),  name='event-calendar-month'),
-    (r'^calendars/(?P<cal_slug>[-\w]+)/$',                                       EventListView.as_view(),   name='event-calendar-detail'),
-    (r'^calendars/$',                                                            ListView.as_view(),        name='event-calendar-list'),
-    
-    (r'^places/$', ListView.as_view(model=Place)), 
-    (r'^places/(?P<slug>[-\w]+)/$', DetailView.as_view(model=Place)), 
-    
-    (r'^$', EventListView.as_view()), 
+    url(r'^calendars/(?P<cal_slug>[-\w]+)/(?P<year>\d{4})/$',                     view=EventYearView.as_view(),   name='event-calendar-year'),
+    url(r'^calendars/(?P<cal_slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/$',  view=EventMonthView.as_view(),  name='event-calendar-month'),
+    url(r'^calendars/(?P<cal_slug>[-\w]+)/$',                                     view=EventListView.as_view(),   name='event-calendar-detail'),
+    url(r'^calendars/$',                                                          view=ListView.as_view(model=Calendar),        name='event-calendar-list'),
+
+    url(r'^places/$', view=ListView.as_view(model=Place), name='place-list'),
+    url(r'^places/(?P<slug>[-\w]+)/$', view=DetailView.as_view(model=Place), name='place-detail'),
+
+    url(r'^(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', view=EventDetailView.as_view(), name='event-detail'),
+    url(r'^(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/$',                  view=EventMonthView.as_view(),  name='event-month'),
+    url(r'^(?P<slug>[-\w]+)/(?P<year>\d{4})/$',                                     view=EventYearView.as_view(),   name='event-year'),
+    url(r'^(?P<slug>[-\w]+)/$',                                                     view=DetailView.as_view(model=Event), name='event-info'),
+
+    url(r'^$', view=EventListView.as_view(), name='event-list'),
 )
-
-

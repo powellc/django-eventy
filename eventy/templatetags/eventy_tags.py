3872b83785ea5a12009eea536eb12d83d3ebb139
Colin Powell <colin@onecardinal.com>
2011-04-04 18:20:20 -0400

    Adding eventy tags and getting the EventCalendar util to work.

---
+++ eventy/templatetags/eventy_tags.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+import logging
+from django.db import models
+from django.conf import settings
+from django import template
+from django.core.urlresolvers import reverse
+from django.utils.dateformat import format
+from eventy.models import Calendar, EventTime
+from eventy.utils import EventCalendar
+
+register = template.Library()
+
+def do_get_next_events(parser, token):
+    ''' Gets a number of upcoming events for a given calendar slug and loads a template variable.
+
+        Example usage:
+
+        {% get_next_events 5 from vacation as events %} 
+        {% get_next_events 5 from all as events %} 
+        {% get_next_events 5 from all but services as events %} 
+        {% get_next_events 5 from all but services,office as events %} 
+
+    '''
+
+    args = token.split_contents()
+    argc = len(args)
+
+    try:
+        assert argc in (6, 8)
+    except AssertionError:
+        raise template.TemplateSyntaxError('Invalid get_committee_groups syntax.')
+    # determine what parameters to use
+    limit = cal_slug = excluded_cals = varname = None
+    if argc == 6: t, limit, f, cal_slug, a, varname = args
+    elif argc == 8: t, limit, f, cal_slug, b, excluded_cals, a, varname = args
+    return GetNextEventsNode(limit=limit, calendar_slug=cal_slug, excluded_cals=excluded_cals, varname=varname)
+
+class GetNextEventsNode(template.Node):
+    def __init__(self, limit, calendar_slug, excluded_cals, varname):
+        self.limit, self.varname, self.calendar_slug, = int(limit), varname, calendar_slug
+        try:
+            self.excluded_cals = excluded_cals.split(',')
+        except:
+            self.excluded_cals = []
+
+    def render(self, context):
+        if self.calendar_slug == 'all':
+            events = EventTime.upcoming_objects.all().order_by('start')
+        else:
+            events = EventTime.upcoming_objects.filter(event__calendar__slug=self.calendar_slug).order_by('start')
+
+        if self.excluded_cals:
+            for cal_slug in self.excluded_cals:
+                events = events.exclude(event__calendar__slug=cal_slug)
+
+        logging.debug('Fetching %s events from calendar %s excluding %s.' % (self.limit, self.calendar_slug, self.excluded_cals))
+        if len(events[:self.limit]) == 1:
+            context[self.varname] = events[0]
+        else:
+            context[self.varname] = events[:self.limit]
+        return ''
+
+register.tag("get_next_events", do_get_next_events)
+
+@register.inclusion_tag('eventy/_month_table.html')
+def show_month(events, date, size="large"):
+    if date == '':
+        month_table = EventCalendar(events, size).formatmonth(datetime.now().year, datetime.now().month)
+    else:
+        month_table = EventCalendar(events, size).formatmonth(date.year, date.month)
+    return {'month_table':month_table, 'size':size }
+
+@register.inclusion_tag('eventy/_year_table.html')
+def show_year(events, date, size="large"):
+    year_table = EventCalendar(events, size).formatyear(date)
+    return {'year_table':year_table }
---
+++ eventy/utils.py
@@ -0,0 +1,50 @@
+import calendar
+import logging
+from datetime import date
+from itertools import groupby
+
+from django.utils.html import conditional_escape as esc
+
+class EventCalendar(calendar.HTMLCalendar):
+    def __init__(self, events, size='large'):
+        super(EventCalendar, self).__init__()
+        self.events = self.group_by_day(events)
+        self.size = size
+    
+    def formatday(self, day, weekday):
+        if day != 0:
+            cssclass = self.cssclasses[weekday]
+            if date.today() == date(self.year, self.month, day):
+                cssclass += ' today'
+            if day in self.events:
+                cssclass += ' filled'
+                if self.size == 'large':
+                    body = ['<ul>']
+                    for event in self.events[day]:
+                        body.append('<li>')
+                        body.append('<a href="%s">' % event.get_absolute_url())
+                        body.append(esc(event.event.title))
+                        body.append('</a></li>')
+                        body.append('</ul>')
+                    return self.day_cell(cssclass, '%d %s' % (day, ''.join(body)))
+                else:
+                    for event in self.events[day]:
+                        body = ['<a href="%s"><div class="%s">%d</div></a>' % (event.get_absolute_url(), event.event.calendar.slug, day)]
+                    return self.day_cell(cssclass, '%s' % ''.join(body))
+            return self.day_cell(cssclass, '%d %s' % (day, ''.join('<div class="noevents"></div>')))
+        return self.day_cell('noday', '&nbsp;')
+
+    def formatmonth(self, year, month):
+        self.year, self.month = year, month
+        return super(EventCalendar, self).formatmonth(year, month, 100)
+
+    def formatyear(self, year):
+        self.year = year
+        return super(EventCalendar, self).formatyear(year)
+
+    def group_by_day(self, events):
+        field = lambda event: event.start.day
+        return dict([(day, list(items)) for day, items in groupby(events, field)])
+
+    def day_cell(self, cssclass, body):
+        return '<td class="%s">%s</td>' % (cssclass, body)
